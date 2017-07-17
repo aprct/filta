2527831e6fe3d3e41bd02b35055fff7d2a3008c8
fei <weekoldsushi@gmail.com>
2017-07-17 10:18:43 -0700

    initial logic

--- __tests__/filta.test.js
+++ __tests__/filta.test.js
@@ -1,5 +1,36 @@
 const filta = require('../index')
 
-test('hello world', () => {
-  expect(1).toBe(1)
+test('should find object with primitive string, number, or boolean prop', () => {
+  const data = [
+    {
+      foo: 'hello world',
+      bar: 0,
+      baz: true
+    },
+    {
+      foo: 'hello snurld',
+      bar: 1,
+      baz: false
+    }
+  ]
+
+  const stringQuery = {
+    foo: 'hello world'
+  }
+  const numberQuery = {
+    bar: 0
+  }
+  const booleanQuery = {
+    baz: true
+  }
+
+  const expected = [{
+    foo: 'hello world',
+    bar: 0,
+    baz: true
+  }]
+
+  expect(filta(data, stringQuery)).toEqual(expected)
+  expect(filta(data, numberQuery)).toEqual(expected)
+  expect(filta(data, booleanQuery)).toEqual(expected)
 })
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const filta = (data, query) => {
-  return {
-    data,
-    query
+const equals = (value1, value2) => {
+  if( typeof value1 !== typeof value2 ) {
+    return false;
   }
+
+  const type = Array.isArray(value1) ? 'array' : typeof value1
+
+  switch(type) {
+    case 'number':
+    case 'string':
+    case 'boolean':
+      return value1 === value2
+
+    default:
+      return false
+  }
+}
+
+const match = (d, query) => {
+  return Object.keys(query)
+    .map(k => equals(query[k], d[k]))
+    .reduce((sum, value) => sum && value)
+}
+
+const filta = (data, query) => {
+  return data
+    .filter(d => match(d, query))
 }
 
 module.exports = filta
